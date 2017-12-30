f4d20d88531b0588097a78ea057b70350cc6a4cd
闫龙 <yan.long@ielpm.com>
2017-12-30 15:31:12 +0800

    初始提交

---
+++ src/main/java/com/asher/blog/BlogApplication.java
@@ -0,0 +1,16 @@
+package com.asher.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * @author yanlong
+ */
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+}
